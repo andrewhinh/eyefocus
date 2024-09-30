232c33762661e447e4b27adc26d8c15562d68c63
Andrew Hinh <ajhinh@gmail.com>
2024-09-30 13:59:47 -0700

    Switch to Molmo for better perf + speed + less GPU issues

--- ft/etl.py
+++ ft/etl.py
@@ -11,7 +11,7 @@ from datasets import load_dataset
 from huggingface_hub import login, snapshot_download
 from PIL import Image
 from tqdm import tqdm
-from transformers import AutoModel, AutoProcessor, BitsAndBytesConfig, MllamaForConditionalGeneration
+from transformers import AutoModel, AutoModelForCausalLM, AutoProcessor, BitsAndBytesConfig, GenerationConfig
 
 from ft.utils import (
     ARTIFACT_PATH,
@@ -32,14 +32,15 @@ src_dir = "self"  # 'ARTIFACT_PATH /' or '/DATA_VOLUME /' + src_dir
 save_dir = "data"
 
 # transform
-model_path = "meta-llama/Llama-3.2-11B-Vision-Instruct"  # "meta-llama/Llama-3.2-90B-Vision-Instruct"
+model_path = "allenai/Molmo-7B-D-0924"  # 'allenai/Molmo-72B-0924'
+dtype = "bf16" if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else "fp16"
 load_in_8bit = False
 load_in_4bit = True
 quant_type = "nf4"
 use_double_quant = True
 
 val_split = 0.1
-max_new_tokens = 1
+max_new_tokens = 5
 prompt = """
 Task: Analyze the given computer screenshot to determine if it shows evidence of focused, productive activity or potentially distracting activity.
 
@@ -65,6 +66,7 @@ Instructions:
 Response Format:
 Return an integer value indicating whether the screenshot primarily shows evidence of distraction or focused activity.
 0 for focused, 1 for distracted.
+Remember, only return an integer value, no additional text or explanations.
 """
 
 # -----------------------------------------------------------------------------
@@ -72,14 +74,14 @@ Return an integer value indicating whether the screenshot primarily shows eviden
 config_keys = [
     k
     for k, v in globals().items()
-    if not k.startswith("_") and isinstance(v, (int, float, str, bool, dict, list, type(None), torch.dtype))
+    if not k.startswith("_") and isinstance(v, (int, float, str, bool, dict, list, type(None)))
 ]
 config = {k: globals()[k] for k in config_keys}  # will be useful for logging
 
 # -----------------------------------------------------------------------------
 
 
-def download_model(is_local) -> tuple[AutoProcessor, AutoModel]:
+def download_model(dtype, is_local) -> tuple[AutoProcessor, AutoModel]:
     login(token=os.getenv("HF_TOKEN"), new_session=not is_local)
 
     local_model_path = snapshot_download(
@@ -90,14 +92,21 @@ def download_model(is_local) -> tuple[AutoProcessor, AutoModel]:
 
     torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
     torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
-    dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
 
-    model = MllamaForConditionalGeneration.from_pretrained(
+    processor = AutoProcessor.from_pretrained(
         local_model_path,
         local_files_only=True,
-        torch_dtype=dtype,
+        torch_dtype="auto",
+        device_map="auto",
+        trust_remote_code=True,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        local_model_path,
+        local_files_only=True,
+        torch_dtype="auto",
         low_cpu_mem_usage=True,
-        attn_implementation="flash_attention_2",
+        # attn_implementation="flash_attention_2",  # TODO: not supported by Molmo
         device_map="auto",
         quantization_config=BitsAndBytesConfig(
             load_in_8bit=load_in_8bit,
@@ -105,36 +114,31 @@ def download_model(is_local) -> tuple[AutoProcessor, AutoModel]:
             bnb_4bit_compute_dtype=dtype,
             bnb_4bit_quant_type=quant_type,
             bnb_4bit_use_double_quant=use_double_quant,
-        ),
+        )
+        if load_in_4bit or load_in_8bit
+        else None,
         trust_remote_code=True,
     )
+    if not load_in_4bit and not load_in_8bit:
+        model = model.to(dtype=dtype)
     model = torch.compile(model)
-
-    processor = AutoProcessor.from_pretrained(
-        local_model_path, local_files_only=True, trust_remote_code=True, use_fast=False
-    )
 
     return processor, model
 
 
 def transform_img(image: Image, processor: AutoProcessor, device) -> torch.Tensor:
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {"type": "image"},
-                {"type": "text", "text": config["prompt"]},
-            ],
-        }
-    ]
-    input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
-    inputs = processor(image, input_text, return_tensors="pt").to(device)
+    inputs = processor.process(
+        images=[image],
+        text=config["prompt"],
+    )
+    inputs = {k: v.to(device).unsqueeze(0) for k, v in inputs.items()}
     return inputs
 
 
 def gen_labels(is_local: bool = False) -> None:
     assert torch.cuda.is_available(), "GPU required"
-    processor, model = download_model(is_local)
+    dtype = torch.float16 if config["dtype"] == "fp16" else torch.bfloat16
+    processor, model = download_model(dtype, is_local)
 
     # check if images are present in src dir
     ## if not load_dataset
@@ -149,26 +153,38 @@ def gen_labels(is_local: bool = False) -> None:
     else:
         images = [Image.open(data_dir / f) for f in os.listdir(data_dir)]
 
+    # set output dir
     if is_local:
         out_dir = ARTIFACT_PATH / save_dir
     else:
         out_dir = Path("/") / DATA_VOLUME / save_dir
     train_idxs = random.sample(range(len(images)), k=math.ceil(len(images) * (1 - config["val_split"])))
 
-    for i in tqdm(range(0, len(images), desc="Processing images")):
-        inputs = transform_img(images[i], processor, model.device)
-        output = model.generate(**inputs, max_new_tokens=config["max_new_tokens"])
-        out = processor.decode(output[0])
+    for i in tqdm(range(0, len(images)), desc="Generating Labels", disable=False):
+        image = images[i]
+        inputs = transform_img(image, processor, model.device)
+        inputs["images"] = inputs["images"].to(dtype)
+        with torch.autocast(
+            device_type=torch.device(model.device).type,
+            enabled=True,
+            dtype=dtype,
+        ):
+            output = model.generate_from_batch(
+                inputs,
+                GenerationConfig(max_new_tokens=config["max_new_tokens"], stop_strings="<|endoftext|>"),
+                tokenizer=processor.tokenizer,
+            )
+        generated_tokens = output[0, inputs["input_ids"].size(1) :]
+        generated_text = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
 
         try:
-            out = int(out)
+            out = int(generated_text)
         except ValueError:
             out = -1
         pred = CLASSES[out] if out >= 0 else "error"
         label_dir = out_dir / "train" / pred if i in train_idxs else out_dir / "validation" / pred
         os.makedirs(label_dir, exist_ok=True)
         img_path = label_dir / f"{i}.jpg"
-        image = images[i]
         if image.mode != "RGB":
             image = image.convert("RGB")
         image.save(img_path)

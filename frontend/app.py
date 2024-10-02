75f8c7dea01629a54c328e2696aef47ff02942fc
Andrew Hinh <ajhinh@gmail.com>
2024-10-01 21:51:39 -0700

    Add toasts + more hdrs + fix style

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,25 @@
-from fasthtml.common import H1, A, Button, Div, Img, P, Script, fast_app, serve
+import os
+from pathlib import Path
 
-tlink = Script(src="https://cdn.tailwindcss.com")
-fasthtml_app, rt = fast_app(ws_hdr=True, hdrs=[tlink])
+from dotenv import load_dotenv
+from fasthtml import common as fh
+
+load_dotenv(Path(__file__).parent.parent / ".env")
+
+tlink = fh.Script(src="https://cdn.tailwindcss.com")
+hjs = fh.HighlightJS(langs=["python", "javascript", "html", "css"])
+fasthtml_app, rt = fh.fast_app(
+    ws_hdr=True, hdrs=[tlink, hjs], live=os.getenv("LIVE", False), debug=os.getenv("DEBUG", False)
+)
+app_name = "Modeldemo"
+fh.setup_toasts(fasthtml_app)
 root_path = "/frontend"
 
 
+# Components
 def github_icon():
-    return A(
-        Img(
+    return fh.A(
+        fh.Img(
             src=f"{root_path}/assets/gh.svg",
             alt="PyPI",
             width="50",
@@ -22,8 +34,8 @@ def github_icon():
 
 
 def pypi_icon():
-    return A(
-        Img(
+    return fh.A(
+        fh.Img(
             src=f"{root_path}/assets/pypi.svg",
             alt="PyPI",
             width="50",
@@ -37,17 +49,21 @@ def pypi_icon():
     )
 
 
+# Layout
 def main_content():
-    return Div(
-        H1("Modeldemo", cls="text-6xl font-bold text-blue-300"),
-        P("Stay focused.", cls="text-xl text-red-500"),
-        Button(
+    return fh.Div(
+        fh.H1("Modeldemo", cls="text-6xl font-bold text-blue-300"),
+        fh.P("Stay focused.", cls="text-xl text-red-500"),
+        fh.Button(
             "uv add modeldemo",
-            onclick="navigator.clipboard.writeText(this.innerText)",
+            onclick="navigator.clipboard.writeText(this.innerText);",
+            hx_post="/toast",
+            hx_target="#toast-container",
+            hx_swap="outerHTML",
             cls="text-blue-300 p-4 rounded text-md hover:bg-zinc-700 hover:text-blue-100 cursor-pointer",
             title="Click to copy",
         ),
-        Div(
+        fh.Div(
             github_icon(),
             pypi_icon(),
             cls="flex gap-8",
@@ -56,10 +72,14 @@ def main_content():
     )
 
 
+def toast_container():
+    return fh.Div(id="toast-container", cls="hidden")
+
+
 def footer():
-    return Div(
-        P("Made by", cls="text-white text-lg"),
-        A(
+    return fh.Div(
+        fh.P("Made by", cls="text-white text-lg"),
+        fh.A(
             "Andrew Hinh",
             href="https://andrewhinh.github.io/",
             cls="text-blue-300 text-lg font-bold hover:text-blue-100",
@@ -68,27 +88,36 @@ def footer():
     )
 
 
+# Routes
 @rt("/")
 async def get():
-    return Div(
+    return fh.Div(
         main_content(),
+        toast_container(),
         footer(),
         cls="flex flex-col justify-between min-h-screen bg-zinc-900 w-full",
     )
 
 
+@rt("/toast")
+async def toast(session):
+    fh.add_toast(session, "Copied to clipboard!", "success")
+    return fh.Div(id="toast-container", cls="hidden")
+
+
+# Serving
 if __name__ == "__main__":
-    serve(app="fasthtml_app")
+    fh.serve(app=app_name)
 
 
-# Serve on Modal
+## Modal
 from modal import App, Image, asgi_app
 
 image = Image.debian_slim(python_version="3.12").pip_install("python-fasthtml")
-app = App("frontend")
+app = App(app_name)
 
 
 @app.function(image=image)
 @asgi_app()
-def get():  # noqa: F811
+def modal_get():
     return fasthtml_app
